06acb201492085ecdc5ec25795760bc1ef65db0b
Viktor Panteleev <me@vpanteleev.com>
2016-10-02 18:11:38 +0200

    Added basic styling

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+    gulp.start(['css']);
 });
 
+gulp.task('css', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass(
+            {includePaths: ['./node_modules/foundation-sites/scss']}
+        ).on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
 
 gulp.task('watch',function() {
     gulp.watch('sass/**/*.scss',['default']);
